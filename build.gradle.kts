06db40d87d459009ce57bb1eef45279f80f7d636
C. Serrano <serrano.cjm@gmail.com>
2022-09-29 13:59:02 -0400

    Adjust safe args

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ buildscript {
         //plugins
         classpath("com.android.tools.build:gradle:4.2.2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.10")
-        classpath("android.arch.navigation:navigation-safe-args-gradle-plugin:1.0.0")
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.6.0-alpha01")
         classpath("org.jlleitschuh.gradle:ktlint-gradle:10.2.0")
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle.kts files
