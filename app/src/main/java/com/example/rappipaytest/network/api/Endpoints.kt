e9d8f6344e291487636de97b9d9fe3b05da6aa87
C. Serrano <serrano.cjm@gmail.com>
2022-10-01 14:03:42 -0400

    Endpoints

--- app/src/main/java/com/example/rappipaytest/network/api/Endpoints.kt
+++ app/src/main/java/com/example/rappipaytest/network/api/Endpoints.kt
@@ -1,4 +1,25 @@
 package com.example.rappipaytest.network.api
 
+import com.example.rappipaytest.data.model.BasicMovieListResponse
+import com.example.rappipaytest.data.model.Movie
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Path
+
 interface Endpoints {
+
+
+    //GET Movie Detail
+    @GET("/movie/{movie_id}")
+    suspend fun getMovieDetail(@Path(value = "movie_id") movieId: Int): Response<Movie?>
+
+    //GET Trending List
+    //option param accepts "day" (for the day) or "week"
+    @GET("/trending/movie/{option}")
+    suspend fun getTrendingMovieList(@Path(value = "option") option: String): Response<BasicMovieListResponse?>
+
+    //GET Upcoming movie list
+    @GET("/movie/upcoming")
+    suspend fun getUpcomingMovieList(): Response<BasicMovieListResponse?>
+
 }
